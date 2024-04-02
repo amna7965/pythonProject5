0df61f08b5553cc149686c4a4a5ccf0279d80edf
HP <amna@7965>
2024-04-02 11:15:48 +0500

    my first python assignment

---
+++ Dates.py
@@ -0,0 +1,22 @@
+# import datetime
+# x= datetime.datetime.now()
+# print(x)
+#
+# import datetime
+# x= datetime . datetime (2024,3,14)
+# print(x)
+# import numpy as np
+#
+# arr = np.array([1, 2, 3, 4])
+#
+# print(arr[0])
+# import numpy as np
+# arr = np.arr({2,4,6,8})
+
+import matplotlib.pyplot as plt
+arr = [2,3,4,10,15]
+plt.plot(arr)
+plt.xlabel('Index')
+plt.ylabel('Value')
+plt.title('My first graph to show data of array')
+plt.show()
